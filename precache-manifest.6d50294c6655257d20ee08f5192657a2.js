0ec56f33bfea4c875ee0c29f5840d9027abd3f8b
Heidi Bada <heidibada@gmail.com>
2019-06-30 22:25:50 -0700

    Updates

---
+++ precache-manifest.6d50294c6655257d20ee08f5192657a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3973e2896bb28fd55fd80d8cd01148",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ad92f90ba1f0552f81a2",
+    "url": "/portfolio/static/css/main.3fe22e23.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/portfolio/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "ad92f90ba1f0552f81a2",
+    "url": "/portfolio/static/js/main.7b3d81d2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
